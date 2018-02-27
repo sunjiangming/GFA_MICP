c198cd71f7d1ab3d2374285d44eb908a27f9a4a4
Jiangming Sun <sunjiangming@gmail.com>
2018-02-27 08:39:35 +0100

    Update do_GFA.sh

--- do_GFA.sh
+++ do_GFA.sh
@@ -1,9 +1,13 @@
+# please set seeds to macau_nompi prior to analysis
 #GE-Mut-CNV-Methylation
 macau_nompi --train both_dim_train_pIC50_fold_1_.sdm --row-prior spikeandslab --col-prior spikeandslab --burnin 1600 --nsamples 401 \
 --num-latent 100 --output-freq 400 --test both_dim_test_pIC50_fold_1_.sdm --output-prefix rst/fold1_genomic_k100_test --row-features ../features/binary/GE.sdm --row-features ../features/binary/Mut.sbm --row-features ../features/binary/Methy.sbm --row-features ../features/binary/CNV.sbm
 
 macau_nompi --train both_dim_train_pIC50_fold_1_.sdm --row-prior spikeandslab --col-prior spikeandslab --burnin 1600 --nsamples 401 \
 --num-latent 100 --output-freq 400 --test both_dim_cal_pIC50_fold_1_.sdm --output-prefix rst/fold1_genomic_k100_cal --row-features ../features/binary/GE.sdm --row-features ../features/binary/Mut.sbm --row-features ../features/binary/Methy.sbm --row-features ../features/binary/CNV.sbm
+
+##run MICP
+python ~/path/micp.py -p fold1_train -t rst/fold1_genomic_k100_test -c rst/fold1_genomic_k100_cal -o $outputdir
 
 #ECFP-Target
 macau_nompi --train both_dim_train_pIC50_fold_1_.sdm --row-prior spikeandslab --col-prior spikeandslab --burnin 1600 --nsamples 401 \
@@ -12,6 +16,8 @@ macau_nompi --train both_dim_train_pIC50_fold_1_.sdm --row-prior spikeandslab --
 macau_nompi --train both_dim_train_pIC50_fold_1_.sdm --row-prior spikeandslab --col-prior spikeandslab --burnin 1600 --nsamples 401 \
 --num-latent 100 --output-freq 400 --test both_dim_cal_pIC50_fold_1_.sdm --output-prefix rst/fold1_chemical_k100_cal  --col-features ../features/binary/ECFP.sbm --col-features ../features/binary/Target.sbm
 
+##run MICP
+python ~/path/micp.py -p fold1_train -t rst/fold1_chemical_k100_test -c rst/fold1_chemical_k100_cal -o $outputdir
 
 #All
 macau_nompi --train both_dim_train_pIC50_fold_1_.sdm --row-prior spikeandslab --col-prior spikeandslab --burnin 1600 --nsamples 401 \
@@ -19,3 +25,6 @@ macau_nompi --train both_dim_train_pIC50_fold_1_.sdm --row-prior spikeandslab --
 
 macau_nompi --train both_dim_train_pIC50_fold_1_.sdm --row-prior spikeandslab --col-prior spikeandslab --burnin 1600 --nsamples 401 \
 --num-latent 100 --output-freq 400 --test both_dim_cal_pIC50_fold_1_.sdm --output-prefix rst/fold1_all_k100_cal  --row-features ../features/binary/GE.sdm --row-features ../features/binary/Mut.sbm --row-features ../features/binary/Methy.sbm --row-features ../features/binary/CNV.sbm --col-features ../features/binary/ECFP.sbm --col-features ../features/binary/Target.sbm
+
+##run MICP
+python ~/path/micp.py -p fold1_train -t rst/fold1_all_k100_test -c rst/fold1_all_k100_cal -o $outputdir
